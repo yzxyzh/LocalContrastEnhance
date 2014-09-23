937930298150fd8c04ad3497816e673b3566db95
yzxyzh <shuxuejinsai@126.com>
2014-09-23 15:02:23 +0800

    Local Contrast Enhancement

--- LocalContrastEnhance/main.cpp
+++ LocalContrastEnhance/main.cpp
@@ -7,12 +7,31 @@
 //
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string fileName="/Users/yanzixu/Desktop/123.png";
+    Mat img=imread(fileName,CV_LOAD_IMAGE_GRAYSCALE);
+    
+    const int kerSize=51;
+    Mat gBlur;
+    cv::GaussianBlur(img, gBlur, cv::Size(kerSize,kerSize), kerSize/3.0);
+    
+    Mat result;
+    cv::addWeighted(img, 1.5, gBlur, -0.5, 0, result);
+    
+    imshow("RESULT",result);
+    waitKey(0);
+    
     return 0;
 }
 
